e6cbae5ea56799ddbb9f460ea99dc8a49f0d675d
audricfullhardt <fullhardtaudric@gmail.com>
2025-11-25 18:11:10 +0100

    feat(balldropper): update to Marius's design

--- Assets/Scripts/MiniGames/BallDropper/Cup.cs
+++ Assets/Scripts/MiniGames/BallDropper/Cup.cs
@@ -5,9 +5,24 @@ public class Cup : MonoBehaviour
 {
     private static int globalScore = 0;
     public TMP_Text scoreText;
+    public Transform rotationCenter;
+    public float rotationSpeedDegreesPerSecond = 70f;
+    public Vector3 rotationAxis = Vector3.up;
     void Start()
     {
         UpdateScoreUI();
+    }
+    void Update()
+    {
+        if (rotationSpeedDegreesPerSecond == 0f || rotationCenter == null)
+            return;
+
+        float deltaDegrees = rotationSpeedDegreesPerSecond * Time.deltaTime;
+        transform.RotateAround(
+            rotationCenter.position,
+            rotationAxis.normalized,
+            deltaDegrees
+        );
     }
     void OnTriggerEnter(Collider other)
     {
--- Assets/Scripts/MiniGames/BallDropper/Platform.cs
+++ Assets/Scripts/MiniGames/BallDropper/Platform.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    [Header("Rotation")]
-    public float rotationSpeedDegreesPerSecond = 70f;
-    public Vector3 localRotationAxis = Vector3.up;
+    [Header("Platform")]
     public GameManager gameManager;
 
     // Start is called before the first frame update
@@ -22,16 +20,5 @@ public class Platform : MonoBehaviour
             gameManager.LoseLife();
             Debug.Log("loose");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (rotationSpeedDegreesPerSecond == 0f)
-        {
-            return;
-        }
-        float deltaDegrees = rotationSpeedDegreesPerSecond * Time.deltaTime;
-        transform.Rotate(localRotationAxis.normalized, deltaDegrees, Space.Self);
     }
 }
